b66b01c95fd8fa6d0d6e54fb97a5059b8c6d8233
Tobias Breitkopf <tobias.breitkopf@googlemail.com>
2022-08-26 19:12:42 +0200

    Loopbakck test

---
+++ Assets/Scripts/LoopBackVisualize.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBackVisualize : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private LoopbackAudio analyser;
+    private List<GameObject> cubes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubes = new List<GameObject>();
+        for (int i = 0; i < analyser.SpectrumData.Length; i++)
+        {
+            cubes.Add(Instantiate(prefab, this.transform));
+            cubes[i].transform.position += new Vector3(i, 0, 0);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            cubes[i].transform.localScale = new Vector3(1, analyser.SpectrumData[i], 1); 
+        }
+    }
+}
